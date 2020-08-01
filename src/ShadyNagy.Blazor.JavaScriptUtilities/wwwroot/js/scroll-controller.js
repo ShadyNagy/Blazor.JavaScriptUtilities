5445cb8eeb07722ee652d0c6382860772f501ae6
Shady Nagy <info@shadynagy.com>
2020-08-01 02:45:55 +0200

    try to run js from lib

---
+++ src/ShadyNagy.Blazor.JavaScriptUtilities/wwwroot/js/scroll-controller.js
@@ -0,0 +1,25 @@
+﻿window.hideScrollOverflowByTag = (tagName) => {
+    if (className === undefined) {
+        document.body.classList.add("blazor-javascript-no-overflow");
+    } else {
+        var elementsByTagName = document.getElementsByTagName(className);
+        for (var tag in elementsByTagName) {
+            if (Object.prototype.hasOwnProperty.call(elementsByTagName, tag)) {
+                tag.classList.add("blazor-javascript-no-overflow");
+            }
+        }
+    }
+};
+
+window.showScrollOverflowByTag = (tagName) => {
+    if (className === undefined) {
+        document.body.classList.remove("blazor-javascript-no-overflow");
+    } else {
+        var elementsByTagName = document.getElementsByTagName(className);
+        for (var tag in elementsByTagName) {
+            if (Object.prototype.hasOwnProperty.call(elementsByTagName, tag)) {
+                tag.classList.remove("blazor-javascript-no-overflow");
+            }
+        }
+    }
+};
